23e5cf4878477d4186c1464481118dc3ad03719d
tranvinhhung <tranvinhung2k@gmail.com>
2020-07-15 16:16:17 +0700

    add1

---
+++ inc/header.php
@@ -0,0 +1,103 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YaMe | Mua Online Quần Áo Thời Trang Nam Nữ Giá Rẻ - YaMe.vn</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Import Boostrap css, js, font awesome here -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="./jquery.js" rel="stylesheet">
+    <!--favicon-->
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+
+        <div class="container-fluid">
+            <a class="navbar-branch" href="index.php">
+                <img src="./images/logo.png" height="65">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+				<span class="navbar-toggler-icon "></span>
+			</button>
+
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto ">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="fast.php">BÁN CHẠY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="sale.php">SALE TẠI CỬA HÀNG</a>
+                    </li>
+                    <li class="nav-item">
+
+                        <div class="dropdown">
+                            <a class="nav-link" href="#" data-toggle="dropdown"> ÁO </a>
+                            <ul class="dropdown-menu">
+                                <a class="dropdown-item" href="aothun.php">Áo thun </a>
+                                <a class="dropdown-item" href="aokhoac.php">Áo khoác </a>
+                                <a class="dropdown-item" href="aosm.php">Áo sơ mi</a>
+
+                            </ul>
+
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+
+                            <a class="nav-link" href="" data-toggle="dropdown">QUẦN</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="quanjean.php">Quần jean</a>
+                                <a class="dropdown-item" href="quantay.php">Quần tây</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="balo.php">BA LÔ      </a>
+                    </li>
+                    <li>
+                        <div class="dropdown show">
+                            <a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fa fa-search"></i>
+                            </a>
+
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <from> <input type="text" name="search">
+                                </from>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="dangnhap.php" class="w3-padding w3-large w3-text-white"><i class="fa fa-user"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" class="w3-padding w3-large w3-text-white"><i class="fa fa-shopping-bag"></i></a>
+                    </li>
+            </div>
+            </ul>
+        </div>
+        </div>
+    </nav>
+</head>
---
+++ inc/info.php
@@ -0,0 +1,12 @@
+<div class="container-fluid padding">
+            <div class="row text-center padding">
+                <div class="col-12">
+                    <h2>FOLLOW US</h2>
+                </div>
+                <div class="col-12 social padding">
+                    <a href="https://www.facebook.com/www.YaMe.vn/"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/yame_vn/?fbclid=IwAR3dkgxQ0J4FNLJgtp4ouf2XQKaUEsfhytOV6Chf6ThiTjTQtAtFIVEYeBg"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.youtube.com/channel/UCi0jrGCcWZBymVWleyWcz4Q"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+</div>
